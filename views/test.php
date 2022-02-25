e030b8370be778e9257b06876a5a7d70f8e5c6bc
Mark <87272164+icypaprik@users.noreply.github.com>
2022-02-25 05:55:20 +0300

    Update test.php

--- views/test.php
+++ views/test.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
 </head>
+    
+<h1>Testing</h1>
+
 <body>
-    <h1>Testing</h1>
 </body>
 </html>
